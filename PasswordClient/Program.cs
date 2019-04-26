a70f32adff0c9e6620583665b6234adfa93fbdd1
Frank Pommerening <frank@pommerening-online.de>
2019-04-26 22:10:23 +0200

    Password-Client

--- PasswordClient/Program.cs
+++ PasswordClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace FP.OAuth.PasswordClient
 {
@@ -8,21 +10,38 @@ namespace FP.OAuth.PasswordClient
     {
         static void Main(string[] args)
         {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:5000/");
-            var request = new HttpRequestMessage(HttpMethod.Post, "auth/token");
 
-            var keyValues = new List<KeyValuePair<string, string>>();
+            AccessToken accessToken = null;
+            using (var tokenclient = new HttpClient())
+            {
+                tokenclient.BaseAddress = new Uri("https://oauth-registry.demo-apps.de");
+                var request = new HttpRequestMessage(HttpMethod.Post, "auth/token");
 
-            keyValues.Add(new KeyValuePair<string, string>("grant_type", "password"));
-            keyValues.Add(new KeyValuePair<string, string>("scope", "offline_access profile email roles"));
-            keyValues.Add(new KeyValuePair<string, string>("resource", "http://localhost:5000"));
-            keyValues.Add(new KeyValuePair<string, string>("username", "user-8041fef86d"));
-            keyValues.Add(new KeyValuePair<string, string>("password", "PW-8ba350344459df8#"));
+                var keyValues = new List<KeyValuePair<string, string>>();
 
-            request.Content = new FormUrlEncodedContent(keyValues);
-            var response = client.SendAsync(request).GetAwaiter().GetResult();
-            var contentAsString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                keyValues.Add(new KeyValuePair<string, string>("grant_type", "password"));
+                keyValues.Add(new KeyValuePair<string, string>("scope", "offline_access profile email roles"));
+                keyValues.Add(new KeyValuePair<string, string>("resource", "https://oauth-resource.demo-apps.de"));
+                keyValues.Add(new KeyValuePair<string, string>("username", "user-81bd13063d"));
+                keyValues.Add(new KeyValuePair<string, string>("password", "PW-3f8e5514c599bd2#"));
+
+                request.Content = new FormUrlEncodedContent(keyValues);
+                var response = tokenclient.SendAsync(request).GetAwaiter().GetResult();
+                var contentAsString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                accessToken = Newtonsoft.Json.JsonConvert.DeserializeObject<AccessToken>(contentAsString);
+            }
+
+            using (var valueclient = new HttpClient())
+            {
+                valueclient.BaseAddress = new Uri( "https://oauth-resource.demo-apps.de");
+                valueclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.access_token);
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(new {value = 42});
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = valueclient.PutAsync($"api/value/", data).GetAwaiter().GetResult();
+                var contentAsString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                
+            }
 
         }
     }
---
+++ PasswordClient/Token.cs
@@ -0,0 +1,17 @@
+﻿namespace FP.OAuth.PasswordClient
+{
+    public class AccessToken
+    {
+
+        public string scope { get; set; }
+
+        public string token_type { get; set; }
+
+        public string access_token { get; set; }
+
+        public int expires_in { get; set; }
+
+        public string refresh_token { get; set; }
+
+    }
+}
