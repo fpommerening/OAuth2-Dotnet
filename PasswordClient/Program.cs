8db1594951cce4c5fdf6a0c50fd17c8f63322105
Frank Pommerening <frank@pommerening-online.de>
2019-04-27 22:03:00 +0200

    Prep

--- PasswordClient/Program.cs
+++ PasswordClient/Program.cs
@@ -11,37 +11,6 @@ namespace FP.OAuth.PasswordClient
         static void Main(string[] args)
         {
 
-            AccessToken accessToken = null;
-            using (var tokenclient = new HttpClient())
-            {
-                tokenclient.BaseAddress = new Uri("https://oauth-registry.demo-apps.de");
-                var request = new HttpRequestMessage(HttpMethod.Post, "auth/token");
-
-                var keyValues = new List<KeyValuePair<string, string>>();
-
-                keyValues.Add(new KeyValuePair<string, string>("grant_type", "password"));
-                keyValues.Add(new KeyValuePair<string, string>("scope", "offline_access profile email roles"));
-                keyValues.Add(new KeyValuePair<string, string>("resource", "https://oauth-resource.demo-apps.de"));
-                keyValues.Add(new KeyValuePair<string, string>("username", "user-81bd13063d"));
-                keyValues.Add(new KeyValuePair<string, string>("password", "PW-3f8e5514c599bd2#"));
-
-                request.Content = new FormUrlEncodedContent(keyValues);
-                var response = tokenclient.SendAsync(request).GetAwaiter().GetResult();
-                var contentAsString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-
-                accessToken = Newtonsoft.Json.JsonConvert.DeserializeObject<AccessToken>(contentAsString);
-            }
-
-            using (var valueclient = new HttpClient())
-            {
-                valueclient.BaseAddress = new Uri( "https://oauth-resource.demo-apps.de");
-                valueclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.access_token);
-                var json = Newtonsoft.Json.JsonConvert.SerializeObject(new {value = 42});
-                var data = new StringContent(json, Encoding.UTF8, "application/json");
-                var response = valueclient.PutAsync($"api/value/", data).GetAwaiter().GetResult();
-                var contentAsString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                
-            }
 
         }
     }
